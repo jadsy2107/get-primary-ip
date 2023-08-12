052e0cfe6e7fd1b9ed7a9cf7f0c8c00959eaa3b9
jadsy2107 <107200645+jadsy2107@users.noreply.github.com>
2023-08-13 09:14:48 +1000

    Create get-primary-ip.sh

---
+++ get-primary-ip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get the default gateway interface
+gateway_interface=$(netstat -rn | awk '$1 == "0.0.0.0" {print $8}')
+
+# Get the IP address for that interface using 'ip addr'
+primary_ip=$(ip addr show $gateway_interface | grep 'inet ' | awk '{print $2}' )
+
+echo "Primary IP Address: $primary_ip"
+
